7ed2ac14e8b2afb2319191f767063b62151ce110
Mathlry <leroy.mathilde.marie@gmail.com>
2020-10-24 22:19:39 +0200

    start adding Identity 12.157

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
 using System;
+using Domain;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,34 +10,35 @@ using Persistence;
 
 namespace API
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+      var host = CreateHostBuilder(args).Build();
+      using (var scope = host.Services.CreateScope())
+      {
+        var services = scope.ServiceProvider;
+        try
         {
-            var host = CreateHostBuilder(args).Build();
-            using( var scope = host.Services.CreateScope() ) 
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    var context = services.GetRequiredService<DataContext>();
-                    context.Database.Migrate();
-                    Seed.SeedData(context);
-                }
-                catch( Exception e )
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(e, "Error during migration.");
-                }
-            }
-            host.Run();
+          var context = services.GetRequiredService<DataContext>();
+          var userManager = services.GetRequiredService<UserManager<AppUser>>();
+          context.Database.Migrate();
+          Seed.SeedData(context, userManager).Wait();
         }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        catch (Exception e)
+        {
+          var logger = services.GetRequiredService<ILogger<Program>>();
+          logger.LogError(e, "Error during migration.");
+        }
+      }
+      host.Run();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+              webBuilder.UseStartup<Startup>();
+            });
+  }
 }
